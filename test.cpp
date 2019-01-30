dd3baac04914ee9f1990abb9b558e01b2286b20e
Dumbledore696 <46421267+Dumbledore696@users.noreply.github.com>
2019-01-30 13:21:04 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,5 +1,28 @@
 #include<stdio.h>
-#include<string.h>
-void main(){
-  printf("Hello World !");
+void main()
+{
+	int count=0;
+	int number,flag=0;
+	scanf("%d",&number);
+	while(number>0)
+	{
+		if(number==1)
+		{
+			flag=1;
+			break;
+		}
+		else if(number%2==0)
+		{
+			count++;
+			number/=2;
+		}
+		else
+		{
+			number=3*number+1;
+		}
+	}
+	if(flag==1)
+		printf("%d",count);
+	else
+		printf("sorry");
 }
